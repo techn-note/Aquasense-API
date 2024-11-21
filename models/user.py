ab42840adac199bf1449a0ee52f3dc3e59393afd
RodrigoS <2005rodrigosilva@gmail.com>
2024-11-21 15:38:50 -0300

    feat: create username request

--- models/user.py
+++ models/user.py
@@ -1,13 +1,16 @@
 from database.db import mongo
+from bson import ObjectId
 
 class User:
     @staticmethod
     def create_user(data):
         return mongo.db.users.insert_one(data)
 
+
     @staticmethod
     def get_user(user_id):
-        return mongo.db.users.find_one({"_id": user_id})
+        return mongo.db.users.find_one({"_id": ObjectId(user_id)})
+
 
     @staticmethod
     def get_user_by_email(email):
--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from services.user_service import create_user_service, loged
 from utils.helpers import response_success, response_error, validate_email
-
+from models.user import User
 
 user_bp = Blueprint('user', __name__)
 
@@ -21,7 +22,6 @@ def add_user():
     return response_success("Usuário foi adicionado com sucesso.", response)
 
 
-
 @user_bp.route('/login', methods=['POST'])
 def login_user():
     data = request.get_json()
@@ -33,3 +33,14 @@ def login_user():
         return response_success("Login successful", {"access_token": access_token})
     
     return response_error("Bad credentials", 400)
+
+
+@user_bp.route('/profile', methods=['GET'])
+@jwt_required()
+def profile():
+    user_id = get_jwt_identity()
+    user = User.get_user(user_id)
+    if user:
+        return response_success("User profile fetched successfully.", {"name": user['name']})
+    else:
+        return response_error("User not found.", 404)
